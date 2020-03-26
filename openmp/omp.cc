9faefb81447c0cfa439116aebb45ee2e5e6da158
gl-coding <1451607278@qq.com>
2020-03-26 19:09:43 +0800

    update

--- openmp/omp.cc
+++ openmp/omp.cc
@@ -1,11 +1,48 @@
 #include <iostream>
 #include <omp.h>
 
-int main()
-{
-#pragma omp parallel for
+int test(){
+    #pragma omp parallel for
     for (char i = 'a'; i <= 'z'; i++)
         std::cout << i << std::endl;
+    return 0;
+}
 
+int test1(){
+    int initValue = 0;
+    int numElements = 100;
+    int array[100] = {0};
+    #pragma omp parallel for
+    for (int i = 0; i  < numElements; i++) {
+        array[i] = initValue;
+        initValue++;
+    }
+}
+
+int test2(){
+    int initValue = 0;
+    int numElements = 100;
+    int array[100];
+    #pragma omp parallel for
+    for (int i = 0; i  < numElements; i++) {
+        array[i] = initValue + i;
+    }
+}
+
+int test3(){
+    int sum = 0;
+    int array[100] = {0};
+    #pragma omp parallel for reduction(+:sum)
+    for (int i = 0; i < 100; i++) {
+        sum += array[i];
+    }
+}
+
+int main()
+{
+    //test();
+    //test1();
+    //test2();
+    test3();
     return 0;
 }
